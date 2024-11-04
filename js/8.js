78757db4495271619e60fa755d16022f19c48834
tomas <toomiileon@gmail.com>
2024-11-04 18:59:54 -0300

    resolvi ejercicio 8

---
+++ js/8.js
@@ -0,0 +1,70 @@
+class Persona {
+  // Propiedades privadas
+  #nombre;
+  #edad;
+  #profesion;
+
+  constructor(nombre, edad, profesion) {
+    this.#nombre = nombre;
+    this.#edad = edad;
+    this.#profesion = profesion;
+  }
+
+  // Métodos get y set para acceder y modificar las propiedades privadas
+  getNombre() {
+    return this.#nombre;
+  }
+
+  setNombre(nombre) {
+    this.#nombre = nombre;
+  }
+
+  getEdad() {
+    return this.#edad;
+  }
+
+  setEdad(edad) {
+    this.#edad = edad;
+  }
+
+  getProfesion() {
+    return this.#profesion;
+  }
+
+  setProfesion(profesion) {
+    this.#profesion = profesion;
+  }
+
+  // Método para saludar
+  saludar() {
+    document.write(
+      `Hola, mi nombre es ${this.#nombre}, tengo ${this.#edad} años y soy ${
+        this.#profesion
+      }.<br>`
+    );
+    console.log(
+      `Hola, mi nombre es ${this.#nombre}, tengo ${this.#edad} años y soy ${
+        this.#profesion
+      }.`
+    );
+  }
+
+  // Método para despedirse
+  despedirse() {
+    document.write(`Adiós, fue un placer.<br>`);
+    console.log(`Adiós, fue un placer.`);
+  }
+}
+
+// Crear dos objetos de la clase Persona con diferentes valores
+const persona1 = new Persona("Tomas", 25, "programamdor");
+const persona2 = new Persona("Ignacio", 22, "constructor");
+
+// Llamar a los métodos de cada objeto
+document.write("Interacción con persona1:<br>");
+persona1.saludar();
+persona1.despedirse();
+
+document.write("<br>Interacción con persona2:<br>");
+persona2.saludar();
+persona2.despedirse();
